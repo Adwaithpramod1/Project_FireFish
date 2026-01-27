83d29edc5fda967da3bdcf34579bd05e6194be2a
Adwaith Pramod <102683482+Adwaithpramod1@users.noreply.github.com>
2026-01-27 14:24:05 +0530

    Fix import formatting in FireFish.py

--- FireFish.py
+++ FireFish.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3                                                      import http.server
+#!/usr/bin/env python3
+import http.server
 import socketserver
 import urllib.parse
 import json
